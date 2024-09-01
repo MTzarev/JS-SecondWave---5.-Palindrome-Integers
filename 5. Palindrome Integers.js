c25fd5364462bc30775e5e6425f59aa891b44403
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-01 22:46:36 +0300

    Add files via upload

---
+++ 5. Palindrome Integers.js
@@ -0,0 +1,16 @@
+function palindrom(arr) {
+
+    let nums = arr.toString().split(`,`);
+    let isPalindrom = true;
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        let reversedNum = num.split(``).reverse().join(``)
+        if (num === reversedNum) {
+            isPalindrom = true;
+        } else {
+            isPalindrom = false;
+        }
+        console.log(isPalindrom);
+    }
+}
+palindrom([32, 2, 232, 1010]);
